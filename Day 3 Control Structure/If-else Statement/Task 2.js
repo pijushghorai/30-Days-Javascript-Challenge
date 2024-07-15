8e0e821d1e540817c6c61b8ef135fc1faacacd1e
Pijush Ghorai <pghorai098@gmail.com>
2024-07-16 00:44:27 +0530

    Task 2 complete

---
+++ Day 3 Control Structure/If-else Statement/Task 2.js
@@ -0,0 +1,10 @@
+function ElegibleforVote (age) {
+    if (age < 18) {
+        console.log("You are not eligible for Vote");
+    } else {
+        console.log("Eligible for Vote");
+    }
+}
+
+ElegibleforVote(24)
+
