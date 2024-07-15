c1f78a99e3213a1aa5ae31136cf34478d63e9439
Pijush Ghorai <pghorai098@gmail.com>
2024-07-16 00:37:44 +0530

    Task one completed

---
+++ Day 3 Control Structure/If-else Statement/Task 1.js
@@ -0,0 +1,11 @@
+function NumberCheck(num) {
+  if (num > 0) {
+    console.log("Number is Positive");
+  } else if (num < 0) {
+    console.log("Number is Nagative");
+  } else {
+    console.log("Number is zero");
+  }
+}
+
+NumberCheck(5);
