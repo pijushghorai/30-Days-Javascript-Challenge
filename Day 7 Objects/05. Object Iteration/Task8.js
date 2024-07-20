940e945fc30aca5f0b5e31faefd799584a252c21
Pijush Ghorai <pghorai098@gmail.com>
2024-07-20 17:02:51 +0530

    complete

---
+++ Day 7 Objects/05. Object Iteration/Task8.js
@@ -0,0 +1,9 @@
+const books = {
+  title: "Ikigai: The Japanese Secret to a Joyful Life",
+  author: "Hector Garcia, Francesc Miralles",
+  year: 2016,
+};
+
+for (const items in books) {
+    console.log(books[items]);
+}
---
+++ Day 7 Objects/05. Object Iteration/Task9.js
@@ -0,0 +1,10 @@
+const books = {
+    title: "Ikigai: The Japanese Secret to a Joyful Life",
+    author: "Hector Garcia, Francesc Miralles",
+    year: 2016,
+  };
+  
+const key = Object.keys(books);
+console.log(key);
+const value = Object.values(books);
+console.log(value);
